621e06b20eebbc8954e0ac2ea4eac25493e8182a
felipe-autentique <felipe@autentique.com.br>
2024-10-21 19:45:54 -0300

    vite hor reloading at supervisord

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -34,6 +34,17 @@ export default function Authenticated({
                                     Dashboard
                                 </NavLink>
                             </div>
+
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink
+                                    href={route('dashboard')}
+                                    active={route().current('dashboard')}
+                                >
+                                    Transactions
+                                </NavLink>
+                            </div>
+
                         </div>
 
                         <div className="hidden sm:ms-6 sm:flex sm:items-center">
