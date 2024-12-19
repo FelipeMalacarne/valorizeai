e28839fb10fff16cf7c2d28d30dbb189265c5420
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-12-19 10:02:34 -0300

    Log SQL on debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.debug')) {
+            DB::listen(function ($query) {
+                Log::info('SQL', [
+                    'query' => $query->sql,
+                    'bindings' => $query->bindings,
+                    'time' => $query->time,
+                ]);
+            });
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
