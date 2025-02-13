c2a9b89ffd0790f366162e20044057452f2ae083
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-02-12 21:26:28 -0300

    Transaction Resource

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->id,
+            'amount'      => $this->amount,
+            'fitid'       => $this->fitid,
+            'memo'        => $this->memo,
+            'currency'    => $this->currency,
+            'description' => $this->description,
+            'categories'  => CategoryResource::collection($this->categories),
+            'date_posted' => $this->date_posted->format('Y-m-d'),
+        ];
+    }
+}
