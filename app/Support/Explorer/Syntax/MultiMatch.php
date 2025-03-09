09a20d680c9350185276817d8a7ba7c56557fbc0
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-03-08 23:28:40 -0300

    Moved explorer from domain to support

--- app/Support/Explorer/Syntax/MultiMatch.php
+++ app/Support/Explorer/Syntax/MultiMatch.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace App\Domain\Explorer\Syntax;
+namespace App\Support\Explorer\Syntax;
 
-use App\Domain\Explorer\Enums\MultiMatchType;
+use App\Support\Explorer\Enums\MultiMatchType;
 use JeroenG\Explorer\Domain\Syntax\MultiMatch as ExplorerMultiMatch;
 
 final class MultiMatch extends ExplorerMultiMatch
