5db319e816f6957ce22f600329462cb0bb19791f
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-03-11 08:51:13 -0300

    Trnasactions Table provider

--- resources/js/Providers/TransactionsTableProvider.tsx
+++ resources/js/Providers/TransactionsTableProvider.tsx
@@ -1,66 +1,41 @@
-// import { DataTableFilterField } from "@/Pages/Transactions/components/Types";
-// import { ColumnDef, ColumnFiltersState, RowSelectionState, SortingState, VisibilityState } from "@tanstack/react-table";
-// import { createContext, useContext, useMemo } from "react";
+import { Transaction } from "@/types";
+import { Table } from "@tanstack/react-table";
+import { createContext, useContext, useMemo } from "react";
 
-// interface Table<T> {
-//     data: T[];
-//     total: number;
-// }
+interface TransactionsTableContextType<TData = any, TValue = any> {
+    table: Table<Transaction>;
+    query: App.Domain.Transaction.Queries.IndexTransactionsQuery;
+    setQuery: (
+        query: App.Domain.Transaction.Queries.IndexTransactionsQuery,
+    ) => void;
+}
 
-// interface DataTableContextType<TData = unknown, TValue = unknown> {
-//     table: Table<TData>;
-//     filterFields: DataTableFilterField<TData>[];
-//     columns: ColumnDef<TData, TValue>[];
-//     enableColumnOrdering: boolean;
-//     isLoading?: boolean;
-//     // REMINDER: controlled state, allowing to rerender the component on state changes
-//     columnFilters: ColumnFiltersState;
-//     sorting: SortingState;
-//     rowSelection: RowSelectionState;
-//     columnOrder: string[];
-//     columnVisibility: VisibilityState;
-// }
+export const DataTableContext = createContext<TransactionsTableContextType<
+    any,
+    any
+> | null>(null);
 
-// export const DataTableContext = createContext<DataTableContextType<
-//     any,
-//     any
-// > | null>(null);
+export function TransactionsTableProvider<TData, TValue>({
+    children,
+    ...props
+}: TransactionsTableContextType<TData, TValue> & {
+    children: React.ReactNode;
+}) {
+    const value = useMemo(() => ({ ...props }), [props.table, props.query]);
 
-// export function DataTableProvider<TData, TValue>({
-//     children,
-//     ...props
-// }: DataTableContextType<TData, TValue> & {
-//     children: React.ReactNode;
-// }) {
-//     const value = useMemo(
-//         () => ({ ...props }),
-//         [
-//             props.columnFilters,
-//             props.sorting,
-//             props.rowSelection,
-//             props.columnOrder,
-//             props.columnVisibility,
-//             props.table,
-//             props.filterFields,
-//             props.columns,
-//             props.enableColumnOrdering,
-//             props.isLoading,
-//         ],
-//     );
+    return (
+        <DataTableContext.Provider value={value}>
+            {children}
+        </DataTableContext.Provider>
+    );
+}
 
-//     return (
-//         <DataTableContext.Provider value={value}>
-//             {children}
-//         </DataTableContext.Provider>
-//     );
-// }
+export function useTransactionsTable<TData, TValue>() {
+    const context = useContext(DataTableContext);
 
-// export function useDataTable<TData, TValue>() {
-//     const context = useContext(DataTableContext);
+    if (!context) {
+        throw new Error("useDataTable must be used within a DataTableProvider");
+    }
 
-//     if (!context) {
-//         throw new Error("useDataTable must be used within a DataTableProvider");
-//     }
-
-//     return context as DataTableContextType<TData, TValue>;
-// }
+    return context as TransactionsTableContextType<TData, TValue>;
+}
