94fca9559b7339ce3e74b59fe68087213ccc65a4
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-03-14 22:16:10 -0300

    fixed
    fixed
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,6 +1,6 @@
 import { Crumb } from "@/components/breadcrumbs";
 import AppLayoutTemplate from "@/layouts/app/app-sidebar-layout";
-import { ThemeProvider } from "@/Providers/ThemeProvider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import { type ReactNode } from "react";
 
 interface AppLayoutProps {
