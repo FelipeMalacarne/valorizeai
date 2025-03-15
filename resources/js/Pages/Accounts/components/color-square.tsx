5ab394d589926c8a90d06501d06bd9ac2d6b4976
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-03-14 22:14:46 -0300

    fixed

--- resources/js/Pages/Accounts/components/color-square.tsx
+++
@@ -1,9 +0,0 @@
-
-export function ColorSquare({ color }: { color: string }) {
-    return (
-        <div className="flex gap-2 items-center">
-            <div className={`h-6 w-6 rounded-md bg-${color}`} />
-            {color.charAt(0).toUpperCase() + color.slice(1)}
-        </div>
-    )
-}
