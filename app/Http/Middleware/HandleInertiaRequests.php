42201b980ff81b6f305165a39c73ee2ddf0023c4
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-06-11 23:12:13 -0300

    Sidebar using localstorage instead of cookie

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -56,7 +56,6 @@ final class HandleInertiaRequests extends Middleware
                 'success' => fn () => $request->session()->get('success'),
                 'error'   => fn () => $request->session()->get('error'),
             ],
-            'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
     }
 }
