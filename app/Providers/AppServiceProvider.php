a5a0266a5ca5980505346bb22c37471d98cb7367
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-07-04 00:50:46 -0300

    App service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,54 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        $this->commands([
-            \App\Console\Commands\MakeActionCommand::class,
-        ]);
+        // ...
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureVite();
+        // $this->configureSanctum();
+        // $this->configureCashier();
     }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
+        Vite::prefetch(concurrency: 3);
+    }
+
+    // private function configureSanctum(): void
+    // {
+    //     Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+    // }
+    //
+    // private function configureCashier(): void
+    // {
+    //     Cashier::useCustomerModel(Organization::class);
+    //     Cashier::useSubscriptionModel(Subscription::class);
+    // }
 }
