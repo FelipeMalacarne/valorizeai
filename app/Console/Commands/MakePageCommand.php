91d344d2234f58dc0cbdffdca0cc35fec90e00d0
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-07-04 00:42:13 -0300

    MakePage command

---
+++ app/Console/Commands/MakePageCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+
+class MakePageCommand extends Command
+{
+    protected $signature = 'make:page {name : The name of the page to create (e.g. accounts/index)}';
+
+    protected $description = 'Create a new Inertia.js page component';
+
+    protected $files;
+
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    public function handle(): int
+    {
+        $name = $this->argument('name');
+
+        $this->generatePage($name);
+
+        return Command::SUCCESS;
+    }
+
+    protected function generatePage(string $name): void
+    {
+        $path = resource_path("js/pages/{$name}.tsx");
+
+        if ($this->files->exists($path)) {
+            $this->error("Page {$name} already exists!");
+            return;
+        }
+
+        $this->makeDirectory($path);
+
+        $stub = $this->files->get($this->getPageStub());
+
+        $className = Str::studly(str_replace('/', ' ', $name));
+        $title = Str::headline(class_basename($name));
+        $breadcrumbTitle = Str::plural(Str::headline(dirname($name)));
+        $breadcrumbRoute = strtolower(Str::plural(dirname($name))) . '.index';
+        $breadcrumbChildTitle = $title;
+        $breadcrumbChildRoute = strtolower(Str::plural(dirname($name))) . '.' . strtolower(Str::kebab(class_basename($name)));
+
+        $stub = str_replace(
+            ['{{className}}', '{{title}}', '{{breadcrumbTitle}}', '{{breadcrumbRoute}}', '{{breadcrumbChildTitle}}', '{{breadcrumbChildRoute}}'],
+            [$className, $title, $breadcrumbTitle, $breadcrumbRoute, $breadcrumbChildTitle, $breadcrumbChildRoute],
+            $stub
+        );
+
+        $this->files->put($path, $stub);
+
+        $this->info("Page [{$path}] created successfully.");
+    }
+
+    protected function getPageStub(): string
+    {
+        return app_path('Console/stubs/inertia-page.stub');
+    }
+
+    protected function makeDirectory(string $path): string
+    {
+        if (!$this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0755, true, true);
+        }
+
+        return $path;
+    }
+}
