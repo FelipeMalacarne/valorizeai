b007c4f08c59d288a344d90c31e6f3b640f220d4
FelipeMalacarne <felipemalacarne012@gmail.com>
2025-09-04 09:58:16 -0300

    Better start container

--- docker/laravel/start-container.sh
+++ docker/laravel/start-container.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 php artisan optimize
 php artisan storage:link
 
-exec php artisan octane:frankenphp --host=0.0.0.0 --port=8080
+# If a command is passed to the script, run it.
+# Otherwise, run the default command.
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+else
+    exec php artisan octane:frankenphp --host=0.0.0.0 --port=8080
+fi
