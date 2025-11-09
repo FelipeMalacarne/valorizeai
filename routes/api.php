f8dd7f88b017d24de419e06e2356f74ba8ff5b21
Felipe Malacarne <felipemalacarne012@gmail.com>
2025-11-08 21:50:54 -0300

    Api routes and doc

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AccountController;
+use App\Http\Controllers\Api\BudgetController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')
+    ->as('api.')
+    ->group(function () {
+        Route::apiResource('accounts', AccountController::class);
+        Route::apiResource('categories', CategoryController::class);
+        Route::apiResource('transactions', TransactionController::class);
+        Route::apiResource('budgets', BudgetController::class);
+    });
